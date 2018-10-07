9affc8ac1f315b17af246af98e691847e3cd07e0
Marc Ramirez <marc.ramirez@billymob.com>
2018-10-07 23:24:29 +0200

    solution for Santa

--- java_scala/src/main/scala/com/intenthq/challenge/SNiceStrings.scala
+++ java_scala/src/main/scala/com/intenthq/challenge/SNiceStrings.scala
@@ -10,7 +10,7 @@ object SNiceStrings {
 //    A nice string is one with all of the following properties:
 //
 //    It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
-//  It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+//    It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
 //    It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
 //    For example:
 //
@@ -21,5 +21,32 @@ object SNiceStrings {
 //    dvszwmarrgswjxmb is naughty because it contains only one vowel.
 //    How many strings are nice?
 
-  def nice(xs: List[String]): Int = ???
+  def nice(xs: List[String]): Int = xs.map(string => isNice(string.toList)).count(_ == true)
+
+  private def isNice(xs: List[Char], vowel: Int = 0, repeat: Boolean = false): Boolean = {
+    xs match {
+      case Nil => sys.error("Sorry, empty list")
+      case last :: Nil => if (isVowel(last)) isLeastThreeVowels(vowel + 1) && repeat else isLeastThreeVowels(vowel) && repeat
+      case first :: rest =>
+        val notAllowedCombinationDetected = isNotAllowed(first + rest.head.toString)
+        if (!notAllowedCombinationDetected){
+          val count = if (isVowel(first)) vowel + 1  else vowel
+          isNice(rest, count, repeat || repeatedCharacter(first, rest.head) )
+        }else
+          false
+    }
+  }
+
+  val disallowedStrings = List("ab", "cd", "pq", "xy")
+  val vowels = List('a', 'e', 'i', 'o', 'u')
+
+  private def isLeastThreeVowels(n: Int): Boolean = n >= 3
+
+  private def isInside[A](c: A, l: List[A]): Boolean = l.contains(c)
+
+  private def isVowel(c: Char): Boolean = isInside(c, vowels)
+
+  private def isNotAllowed(pair: String): Boolean = isInside(pair, disallowedStrings)
+
+  private def repeatedCharacter(c: Char, cc: Char): Boolean = c == cc
 }
